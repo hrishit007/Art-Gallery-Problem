88c6c98f97aeef615fdeee2834a12d463fa788e7
hrishit007 <hrishit.bp@gmail.com>
2024-09-30 13:08:24 +0530

    Defining macros

--- cp1.cpp
+++ cp1.cpp
@@ -17,12 +17,35 @@ struct tVertexStructure{
     tPoint coords;
     bool ear;
     tVertex next, prev;
+    //TODO: Add incident edge
 };
 
 tVertex vertices = NULL;
 
+#define NEW(p, type)\
+    if((p = (type*)malloc(sizeof(type))) == NULL){\
+        fprintf(stderr, "NEW: Out of memory\n");\
+        exit(EXIT_FAILURE);\
+    }
+
+#define ADD(head, p)\
+    if (head == NULL){\
+        p->next = p;\
+        p->prev = p;\
+        head = p;\
+    }\
+    else{\
+        p->next = head;\
+        p->prev = head->prev;\
+        head->prev->next = p;\
+        head->prev = p;\
+    }   
+
+#define FREE(p) if(p) {free((char*)p); p = NULL;}
+    
 using namespace std;
 
 int main(){
+
     
 }
